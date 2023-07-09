c2b7aadcaaf8ca0c0f2a254f46b774cb54976bef
techiecream <34246365+techiecream@users.noreply.github.com>
2023-07-09 11:46:19 +0300

    A Music player
    Woke up feeling like listening to music, my network was exceptionaly poor. Spotify pissed me off so i decided to design code to play for me my songs basing on the mood

---
+++ MusicPlayer.py
@@ -0,0 +1,85 @@
+import os
+import librosa
+import pygame.mixer
+import datetime
+import random
+import time
+
+# Directory containing the MP3 files
+directory = os.getcwd()
+
+# Initialize Pygame mixer
+pygame.mixer.init()
+
+# Set the desired playback speed factor
+playback_speed_factor = 1.0
+
+# Define BPM ranges for different times of day
+bpm_ranges = {
+    'morning': (60, 90),  # Slow music for morning
+    'afternoon': (90, 120),  # Upbeat music for afternoon
+    'evening': (60, 90),  # Relaxing music for evening
+}
+
+# Get the current time of day
+current_time = datetime.datetime.now()
+current_hour = current_time.hour
+
+# Determine the time range based on the current hour
+if 6 <= current_hour < 12:
+    time_range = 'morning'
+elif 12 <= current_hour < 18:
+    time_range = 'afternoon'
+else:
+    time_range = 'evening'
+
+# List to store the songs within the BPM range for the current time of day
+optimal_songs = []
+
+# Total time in seconds
+total_time = 0
+
+# Iterate through all the files in the directory to get the BPM
+for filename in os.listdir(directory):
+    if filename.endswith('.mp3'):
+        file_path = os.path.join(directory, filename)
+
+        # Load the audio file and get the tempo
+        audio, _ = librosa.load(file_path)
+        tempo, _ = librosa.beat.beat_track(y=audio)
+
+        # Check if the current song is within the BPM range for the current time of day
+        if bpm_ranges[time_range][0] <= tempo <= bpm_ranges[time_range][1]:
+            optimal_songs.append(file_path)
+            # Calculate the duration of the song
+            duration = librosa.get_duration(y=audio)
+            total_time += duration
+
+# Shuffle the optimal songs
+random.shuffle(optimal_songs)
+
+# Display the songs to be played and their BPM values
+for song_path in optimal_songs:
+    filename = os.path.basename(song_path)
+    print("Now playing: {}, Tempo: {} BPM".format(filename, tempo))
+
+# Display the total time in minutes
+print("Total Time: {:.2f} minutes".format(total_time / 60))
+
+# Play the shuffled optimal songs with crossfade
+for i, song_path in enumerate(optimal_songs):
+    # Load the MP3 file into Pygame mixer and play it
+    pygame.mixer.music.load(song_path)
+    pygame.mixer.music.play(fade_ms=0)  # Start playing without fade-in
+    
+    if i > 0:
+        # Crossfade with the previous song
+        pygame.mixer.music.fadeout(15000)  # Fade out the previous song over 15 seconds
+        time.sleep(15)  # Wait for the crossfade duration
+        
+    # Wait for the song to finish playing
+    while pygame.mixer.music.get_busy():
+        pass
+
+# Quit Pygame mixer
+pygame.mixer.quit()
